cbf94133dfbb1683211ac3334c3905f82a4e2e5a
Kei Takagi <sanuki.udon.kamatama@gmail.com>
2017-10-29 09:38:11 +0900

    キーリピートの廃止
    処理が重いと受信バケットを取り逃がすため、キーを離したタイミングがとれない
    テスト機以外の構成でキーを押したときのチャタリングが起きやすい

--- ttbasic_v85_difference/ps22tty.cpp
+++ ttbasic_v85_difference/ps22tty.cpp
@@ -6,15 +6,7 @@
 #include <XboxChatpad.h>
 #include "tTVscreen.h"
 
-//ボタンを押してから2個目の文字が表示するまでのカウント
-#define REPEATCNT1 120000
-//ボタンのリピートカウント
-#define REPEATCNT2 10000
-
 XboxChatpad kb;
-static boolean keyDown = false;
-static char keyCode = 0;
-static uint32_t Nexttime;
 
 void setupPS2(uint8_t kb_type = false) {
   // チャットパッドのセットアップ
@@ -31,22 +23,7 @@ uint8_t ps2read() {
   keyEvent k; // キー入力情報 
   k = kb.read();
   if ( k.code && k.code != KEY_ERROR) {
-    if (k.BREAK==0){
-      //キーを押した
-      keyDown = true;
-      keyCode = k.code;
-      c = keyCode;
-      Nexttime = REPEATCNT1;
-    }else{
-      //キーを離した
-      keyDown = false;
-      keyCode = 0; 
-    }
-  }
-  if(0<Nexttime)Nexttime--;
-  if(keyDown == true && Nexttime == 0){
-    c = keyCode;
-    Nexttime=REPEATCNT2;
+    if (k.BREAK==1)c = k.code;//キーを離した
   }
   return c;
 }
